3ee4f691a5cc724bc6ea03a5b0e650495ff07da8
rm <biffbangpow767@yahoo.com>
2017-08-18 11:47:27 -0700

    commit my progress up until lunchtime

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,18 @@ $(document).ready(function() {
     var userInput = parseInt($("#userInput").val());
     var result = $("#result");
     var numbers = [];
+    var ping = "ping";
     for (var i=1; i <= 10; i++) {
       numbers.push(i)
     }
-    numbers.splice(0, userInput);
+    numbers.forEach(function(three) {
+      if (three % 3 === 0) {
+        three = numbers.toString("ping");
+      }
+      return three;
+    })
+    var sliced = numbers.slice(0, userInput);
     result.show();
-    result.append(numbers);
+    result.append(sliced);
   });
 });
