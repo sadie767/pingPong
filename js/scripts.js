6d87f4dc9383c78fffc983147a58665b637093d5
rm <biffbangpow767@yahoo.com>
2017-08-18 10:38:57 -0700

    commit my haberdashing of slicing and splicing

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#userInput").val();
-    alert(userInput);
-    $("#result").show(userInput);
+    var userInput = parseInt($("#userInput").val());
+    var result = $("#result");
+    var numbers = [];
+    for (var i=1; i <= 10; i++) {
+      numbers.push(i)
+    }
+    numbers.splice(0, userInput);
+    result.show();
+    result.append(numbers);
   });
 });
