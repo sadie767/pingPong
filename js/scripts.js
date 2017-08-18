a3610a6d75fedb28174a154cebee4f98f9d8a685
rm <biffbangpow767@yahoo.com>
2017-08-18 13:21:03 -0700

    commit working code with all of the specs minus being able to display the results on the website/html.

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,26 @@ $(document).ready(function() {
     var userInput = parseInt($("#userInput").val());
     var result = $("#result");
     var numbers = [];
-    var ping = "ping";
     for (var i=1; i <= 10; i++) {
       numbers.push(i)
     }
     numbers.forEach(function(three) {
       if (three % 3 === 0) {
-        three = numbers.toString("ping");
+        three = "ping";
       }
       return three;
+    })
+    numbers.forEach(function(five) {
+      if (five % 5 === 0) {
+        five = "pong";
+      }
+      return five;
+    })
+    numbers.forEach(function(both) {
+      if (both % 3 === 0 && both % 5 === 0) {
+        both = "ping-pong";
+      }
+      return both;
     })
     var sliced = numbers.slice(0, userInput);
     result.show();
