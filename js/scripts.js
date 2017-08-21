205e68f45b40561c9271308cef0c28bd54f04b62
Guest User <Guest@Epicodus-9D.local>
2017-08-21 09:11:43 -0700

    demonstrate my final project with an image for fun

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 //business inter-face
 
 var pingPong = function(userInput) {
-var numbsNping = [];
-for (var i=1; i <= userInput; i++) {
-  if ((i % 3 === 0) && (i % 5 === 0)) {
-    var splice = numbsNping.splice(i-1, 1)
-    var string = i.toString();
-    string = "pingpong";
-    numbsNping.push(string);
-  } else if (i % 3 === 0) {
-    var splice = numbsNping.splice(i-1, 1);
-    var string = i.toString();
-    var string = "ping";
-    numbsNping.push(string);
-  } else if (i % 5 === 0) {
-    var splice = numbsNping.splice(i-1, 1);
-    var string = i.toString
-    var string = "pong";
-    numbsNping.push(string);
-  }
-  else {
-    var splice = numbsNping.splice(i-1, 1);
-    var string = i.toString
-    string = i;
-    numbsNping.push(i);
-  }
-    $("#result").append("<li>" + string + "</li>")
-}
+  var numbsNping = [];
+  for (var i=1; i <= userInput; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      var splice = numbsNping.splice(i-1, 1)
+      var string = i.toString();
+      string = "pingpong";
+      numbsNping.push(string);
+      } else if (i % 3 === 0) {
+      var splice = numbsNping.splice(i-1, 1);
+      var string = i.toString();
+      var string = "ping";
+      numbsNping.push(string);
+      } else if (i % 5 === 0) {
+      var splice = numbsNping.splice(i-1, 1);
+      var string = i.toString
+      var string = "pong";
+      numbsNping.push(string);
+      } else {
+      var splice = numbsNping.splice(i-1, 1);
+      var string = i.toString
+      string = i;
+      numbsNping.push(i);
+    }
+      $("#result").append("<li>" + string + "</li>")
+    }
 }
 
 //user inter-face
