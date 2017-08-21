e4fd252313b6e98cd37e5504e9b07b42d2c560ce
rm <biffbangpow767@yahoo.com>
2017-08-20 22:56:39 -0600

    demonstrate a commit that separates business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,38 @@
+//business inter-face
+
+var pingPong = function(userInput) {
+var numbsNping = [];
+for (var i=1; i <= userInput; i++) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    var splice = numbsNping.splice(i-1, 1)
+    var string = i.toString();
+    string = "pingpong";
+    numbsNping.push(string);
+  } else if (i % 3 === 0) {
+    var splice = numbsNping.splice(i-1, 1);
+    var string = i.toString();
+    var string = "ping";
+    numbsNping.push(string);
+  } else if (i % 5 === 0) {
+    var splice = numbsNping.splice(i-1, 1);
+    var string = i.toString
+    var string = "pong";
+    numbsNping.push(string);
+  }
+  else {
+    numbsNping.push(i);
+  }
+    $("#result").append("<li>" + numbsNping + "</li>")
+}
+}
+
+//user inter-face
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    var numbsNping = [];
-    var result = $("#result");
-    for (var i=1; i <= userInput; i++) {
-      if ((i % 3 === 0) && (i % 5 === 0)) {
-        var splice = numbsNping.splice(i-1, 1)
-        var string = i.toString();
-        string = "pingpong";
-        numbsNping.push(string);
-      } else if (i % 3 === 0) {
-        var splice = numbsNping.splice(i-1, 1);
-        var string = i.toString();
-        var string = "ping";
-        numbsNping.push(string);
-      } else if (i % 5 === 0) {
-        var splice = numbsNping.splice(i-1, 1);
-        var string = i.toString
-        var string = "pong";
-        numbsNping.push(string);
-      }
-      else {
-        numbsNping.push(i);
-      }
-        $("#result").append("<li>" + numbsNping + "</li>")
-    }
-    result.show(numbsNping);
+    var result = pingPong(userInput);
+    $("#result").show (result);
   });
 });
