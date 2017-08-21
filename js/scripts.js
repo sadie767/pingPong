d4381222c326262cf28b79857552ce890175c4e7
rm <biffbangpow767@yahoo.com>
2017-08-20 19:54:04 -0600

    display my project almost in working state. The whole of the array is pushing into my ordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,30 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    var numbers = [];
-    for (var i=1; i <= 100; i++) {
-      numbers.push(i);
+    var numbsNping = [];
+    var result = $("#result");
+    for (var i=1; i <= userInput; i++) {
       if ((i % 3 === 0) && (i % 5 === 0)) {
-        var splice = numbers.splice(i-1, 1);
-        var string = i.toString
-        string = "pingpong";
-        numbers.push(string);
-      } else if (i % 3 === 0) {
-        var splice = numbers.splice(i-1, 1);
+        var splice = numbsNping.splice(i-1, 1)
         var string = i.toString();
-        string = "ping";
-        numbers.push(string);
+        string = "pingpong";
+        numbsNping.push(string);
+      } else if (i % 3 === 0) {
+        var splice = numbsNping.splice(i-1, 1);
+        var string = i.toString();
+        var string = "ping";
+        numbsNping.push(string);
       } else if (i % 5 === 0) {
-        var splice = numbers.splice(i-1, 1);
+        var splice = numbsNping.splice(i-1, 1);
         var string = i.toString
-        string = "pong";
-        numbers.push(string);
-          }
-          else if ((i % 3 === 0) && (i % 5 === 0)) {
-            var splice = numbers.splice(i-1, 1);
-            var string = i.toString
-            string = "pingpong";
-            numbers.push(string);
-              }
-        var numberSliced = numbers.slice(0, userInput);
-        var result = $("#result");
-        $("#result").append("<li>" + i + "</li>");
-        }
-    result.show(numberSliced);
-    result.append(numberSliced);
+        var string = "pong";
+        numbsNping.push(string);
+      }
+      else {
+        numbsNping.push(i);
+      }
+        $("#result").append("<li>" + numbsNping + "</li>")
+    }
+    result.show(numbsNping);
   });
 });
